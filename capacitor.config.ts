fb2aec8fff7be03b411b17eb4d3d3d38b2d9ffda
Nikos Paraskevopoulos <nikospara@yahoo.com>
2025-11-24 22:46:25 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+	appId: 'eu.dietwise.recipewatch',
+	appName: 'Recipe Watch',
+	webDir: 'dist'
+};
+
+export default config;
