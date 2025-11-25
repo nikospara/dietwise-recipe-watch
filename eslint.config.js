75a6a49ccafc9d5a3d8d01fc44547951c2e7ddf7
Nikos Paraskevopoulos <nikospara@yahoo.com>
2025-11-26 00:27:43 +0200

    Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -4,11 +4,15 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import recommendedConfig from 'eslint-plugin-prettier/recommended'
 
 export default defineConfig(
 	{ ignores: ['dist', 'cypress.config.ts'] },
 	{
-		extends: [js.configs.recommended, ...tseslint.configs.recommended],
+		extends: [
+			js.configs.recommended,
+			...tseslint.configs.recommended
+		],
 		files: ['**/*.{ts,tsx}'],
 		languageOptions: {
 			ecmaVersion: 2020,
@@ -28,4 +32,5 @@ export default defineConfig(
 			'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		},
 	},
+	recommendedConfig,
 )
