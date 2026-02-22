5ee7f685a253c94861566213b8f8f82162f1c38d
Nikos Paraskevopoulos <nikospara@yahoo.com>
2026-02-22 18:50:04 +0200

    Refactored the reducers of SuggestionStatusAction into their own file

--- src/recipe/reducer.ts
+++ src/recipe/reducer.ts
@@ -1,6 +1,7 @@
 import type { MainAction } from './actions';
 import type { MainData, SuggestionState } from './model';
 import { v4 as uuidv4 } from 'uuid';
+import { acceptedSuggestion, rejectedSuggestion, undecided } from 'recipe/reducers/reduceSuggestionStatusAction';
 
 export function createInitialState(): MainData {
 	return {
@@ -127,113 +128,21 @@ export function reducer(state: MainData, action: MainAction): MainData {
 			const oldStatus = target.status;
 			const newStatus = action.status;
 			if (oldStatus !== newStatus) {
-				const newSuggestionState: SuggestionState = {
-					...target,
-					status: newStatus,
-				};
 				switch (newStatus) {
 					case 'ACCEPTED': {
-						if (target.suggestion.target.type === 'INGREDIENT') {
-							const targetIngredientId = target.suggestion.target.ingredient;
-							let newSuggestions = state.suggestions;
-							// deactivate the previous selected for the same ingredient
-							const previouslySelectedSuggestionId = state.ingredientState?.[targetIngredientId];
-							if (previouslySelectedSuggestionId) {
-								const previouslySelectedSuggestion =
-									state.suggestions?.[previouslySelectedSuggestionId];
-								if (!previouslySelectedSuggestion) {
-									throw new Error(
-										`The previous suggestion for ingredient does not exist: i: ${targetIngredientId}, s: ${previouslySelectedSuggestionId}`,
-									);
-								}
-								newSuggestions = {
-									...newSuggestions,
-									[previouslySelectedSuggestionId]: {
-										...previouslySelectedSuggestion,
-										status: 'UNDECIDED',
-									},
-								};
-							}
-							// mark the new suggestion as accepted
-							newSuggestions = {
-								...newSuggestions,
-								[action.id]: newSuggestionState,
-							};
-							// assign the suggestion to the ingredient
-							const newIngredientState = {
-								...state.ingredientState,
-								[targetIngredientId]: action.id,
-							};
-							return {
-								...state,
-								suggestions: newSuggestions,
-								ingredientState: newIngredientState,
-							};
-						} else {
-							// no RECIPE-level suggestions so far
-							return state;
-						}
+						return acceptedSuggestion(state, action, target);
 					}
 					case 'REJECTED': {
-						if (target.suggestion.target.type === 'INGREDIENT') {
-							const targetIngredientId = target.suggestion.target.ingredient;
-							// mark the new suggestion
-							const newSuggestions = {
-								...state.suggestions,
-								[action.id]: newSuggestionState,
-							};
-							// assign undefined to the ingredient, only if previously related to this suggestion
-							let newIngredientState = state.ingredientState;
-							const previouslySelectedSuggestionId = state.ingredientState?.[targetIngredientId];
-							if (previouslySelectedSuggestionId === action.id) {
-								newIngredientState = {
-									...state.ingredientState,
-									[targetIngredientId]: undefined,
-								};
-							}
-							return {
-								...state,
-								suggestions: newSuggestions,
-								ingredientState: newIngredientState,
-							};
-						} else {
-							// no RECIPE-level suggestions so far
-							return state;
-						}
+						return rejectedSuggestion(state, action, target);
 					}
 					case 'UNDECIDED': {
-						if (target.suggestion.target.type === 'INGREDIENT') {
-							const targetIngredientId = target.suggestion.target.ingredient;
-							// mark the new suggestion
-							const newSuggestions = {
-								...state.suggestions,
-								[action.id]: newSuggestionState,
-							};
-							// assign undefined to the ingredient, only if previously related to this suggestion
-							let newIngredientState = state.ingredientState;
-							const previouslySelectedSuggestionId = state.ingredientState?.[targetIngredientId];
-							if (previouslySelectedSuggestionId === action.id) {
-								newIngredientState = {
-									...state.ingredientState,
-									[targetIngredientId]: undefined,
-								};
-							}
-							return {
-								...state,
-								suggestions: newSuggestions,
-								ingredientState: newIngredientState,
-							};
-						} else {
-							// no RECIPE-level suggestions so far
-							return state;
-						}
+						return undecided(state, action, target);
 					}
 				}
 			} else {
 				return state;
 			}
-			// XXX
-			// throw new Error('Illegal fallthrough'); // TS claims the default below is illegal without this; WHY?!?!
+			// throw new Error('Illegal fallthrough'); // XXX VS Code claims the default below is illegal without this!!!
 		}
 		// see https://www.typescriptlang.org/docs/handbook/2/narrowing.html#exhaustiveness-checking
 		default: {
---
+++ src/recipe/reducers/reduceSuggestionStatusAction.ts
@@ -0,0 +1,112 @@
+import type { SuggestionStatusAction } from 'recipe/actions';
+import type { MainData, SuggestionState } from 'recipe/model';
+
+export function acceptedSuggestion(state: MainData, action: SuggestionStatusAction, target: SuggestionState): MainData {
+	if (target.suggestion.target.type === 'INGREDIENT') {
+		const newSuggestionState: SuggestionState = {
+			...target,
+			status: 'ACCEPTED',
+		};
+		const targetIngredientId = target.suggestion.target.ingredient;
+		let newSuggestions = state.suggestions;
+		// deactivate the previous selected for the same ingredient
+		const previouslySelectedSuggestionId = state.ingredientState?.[targetIngredientId];
+		if (previouslySelectedSuggestionId) {
+			const previouslySelectedSuggestion = state.suggestions?.[previouslySelectedSuggestionId];
+			if (!previouslySelectedSuggestion) {
+				throw new Error(
+					`The previous suggestion for ingredient does not exist: i: ${targetIngredientId}, s: ${previouslySelectedSuggestionId}`,
+				);
+			}
+			newSuggestions = {
+				...newSuggestions,
+				[previouslySelectedSuggestionId]: {
+					...previouslySelectedSuggestion,
+					status: 'UNDECIDED',
+				},
+			};
+		}
+		// mark the new suggestion as accepted
+		newSuggestions = {
+			...newSuggestions,
+			[action.id]: newSuggestionState,
+		};
+		// assign the suggestion to the ingredient
+		const newIngredientState = {
+			...state.ingredientState,
+			[targetIngredientId]: action.id,
+		};
+		return {
+			...state,
+			suggestions: newSuggestions,
+			ingredientState: newIngredientState,
+		};
+	} else {
+		// no RECIPE-level suggestions so far
+		return state;
+	}
+}
+
+export function rejectedSuggestion(state: MainData, action: SuggestionStatusAction, target: SuggestionState): MainData {
+	if (target.suggestion.target.type === 'INGREDIENT') {
+		const newSuggestionState: SuggestionState = {
+			...target,
+			status: 'REJECTED',
+		};
+		const targetIngredientId = target.suggestion.target.ingredient;
+		// mark the new suggestion
+		const newSuggestions = {
+			...state.suggestions,
+			[action.id]: newSuggestionState,
+		};
+		// assign undefined to the ingredient, only if previously related to this suggestion
+		let newIngredientState = state.ingredientState;
+		const previouslySelectedSuggestionId = state.ingredientState?.[targetIngredientId];
+		if (previouslySelectedSuggestionId === action.id) {
+			newIngredientState = {
+				...state.ingredientState,
+				[targetIngredientId]: undefined,
+			};
+		}
+		return {
+			...state,
+			suggestions: newSuggestions,
+			ingredientState: newIngredientState,
+		};
+	} else {
+		// no RECIPE-level suggestions so far
+		return state;
+	}
+}
+
+export function undecided(state: MainData, action: SuggestionStatusAction, target: SuggestionState): MainData {
+	if (target.suggestion.target.type === 'INGREDIENT') {
+		const newSuggestionState: SuggestionState = {
+			...target,
+			status: 'UNDECIDED',
+		};
+		const targetIngredientId = target.suggestion.target.ingredient;
+		// mark the new suggestion
+		const newSuggestions = {
+			...state.suggestions,
+			[action.id]: newSuggestionState,
+		};
+		// assign undefined to the ingredient, only if previously related to this suggestion
+		let newIngredientState = state.ingredientState;
+		const previouslySelectedSuggestionId = state.ingredientState?.[targetIngredientId];
+		if (previouslySelectedSuggestionId === action.id) {
+			newIngredientState = {
+				...state.ingredientState,
+				[targetIngredientId]: undefined,
+			};
+		}
+		return {
+			...state,
+			suggestions: newSuggestions,
+			ingredientState: newIngredientState,
+		};
+	} else {
+		// no RECIPE-level suggestions so far
+		return state;
+	}
+}
